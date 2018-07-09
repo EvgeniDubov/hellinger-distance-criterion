94948a1de529880279c43f1fa7954eb499ac157e
EvgeniDubov <32032278+EvgeniDubov@users.noreply.github.com>
2018-07-09 22:21:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 	name='hellinger-distance-criterion',
 	version=0.1,
 	url='github.com/EvgeniDubov/hellinger-random-forest',
-    author='Evgeni Dubov',
+    	author='Evgeni Dubov',
 	author_email='evgeni.dubov@gmail.com',
 	ext_modules = cythonize('hellinger_distance_criterion.pyx'),
 	include_dirs=[numpy.get_include()]
